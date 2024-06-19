addb793ff51e43c64e42520fa9d8f29128511330
Shahrukh98 <shahrukhnawaz98@gmail.com>
2024-06-19 23:00:22 +0500

    env names fixed

--- generate_auth_url.py
+++ generate_auth_url.py
@@ -8,11 +8,11 @@ def generate_auth_url():
     """Generate and return the OAuth2 authorization URL for LinkedIn."""
 
     base_url = "https://www.linkedin.com/oauth/v2/authorization"
-    client_id = os.getenv("LINKEDIN_CLIENT_ID")
-    redirect_uri = os.getenv("LINKEDIN_REDIRECT_URI")
+    client_id = os.getenv("CLIENT_ID")
+    redirect_uri = os.getenv("REDIRECT_URI")
     ## Since we might have multiple scopes with spaces between, so we need to convert scope
-    scope = quote(os.getenv("LINKEDIN_SCOPES")) 
-    state = os.getenv("LINKEDIN_STATE")
+    scope = quote(os.getenv("SCOPES")) 
+    state = os.getenv("STATE")
     response_type = "code"
 
     url = f"{base_url}?client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}&state={state}&response_type={response_type}"
